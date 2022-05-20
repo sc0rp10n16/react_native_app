6f3b034f1ab43cea55a1cf3705583f0318187d19
Akshith Mysa <akshithmysa.physics@gmail.com>
2022-05-21 00:42:53 +0530

    InitialSetup

---
+++ screens/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Details = () => {
+    return (
+        <Text>Details</Text>
+    )
+}
+
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Home = () => {
+    return (
+        <Text>Home</Text>
+    )
+}
+
+export default Home
