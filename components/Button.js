8714313f80715e1b41d4537cb78b62d871482c86
Akshith Mysa <akshithmysa.physics@gmail.com>
2022-05-24 10:09:21 +0530

    Home page is done

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
-import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+import { COLORS, SIZES, SHADOWS, assets, FONTS } from '../constants';
 import React from 'react'
 
 export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
@@ -26,10 +26,36 @@ export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
         </TouchableOpacity>
     )
 }
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
     return (
-        <View>
-            <Text>RectButton</Text>
-        </View>
+        // <TouchableOpacity
+        //     style={{
+        //         backgroundColor: COLORS.primary,
+        //         borderRadius: SIZES.extraLarge,
+        //         minWidth: minWidth,
+        //         padding: SIZES.small,
+
+        //         ...props,
+        //     }}
+        //     onPress={handlePress}
+        // >
+        //     <Text style={{
+        //         fontFamily: FONTS.semiBold,
+        //         fontSize: fontSize,
+        //         color: COLORS.white,
+        //     }}>Place a bid</Text>
+        // </TouchableOpacity>
+        <TouchableOpacity
+            style={{
+                backgroundColor: COLORS.primary,
+                borderRadius: SIZES.extraLarge,
+                minWidth: minWidth,
+                padding: SIZES.small,
+                ...props,
+            }}
+            onPress={handlePress}
+        >
+            <Text style={{ fontFamily: FONTS.semiBold, fontSize: fontSize, color:COLORS.white, textAlign: 'center'}}>Place a bid</Text>
+        </TouchableOpacity>
     )
 }
